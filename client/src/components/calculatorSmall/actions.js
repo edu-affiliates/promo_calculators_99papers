47c629d19651ec97b0f229c3245efd65527fd4d7
edu-affiliates <mango@plasticjam.com>
2017-07-06 12:02:45 +0300

    add fetch actions

--- client/src/components/calculatorSmall/actions.js
+++ client/src/components/calculatorSmall/actions.js
@@ -4,6 +4,9 @@
 export const PLUS_PAGE = 'PLUS_PAGE';
 export const MINUS_PAGE = 'MINUS_PAGE';
 export const CHANGE_SERVICE = 'CHANGE_SERVICE';
+export const FETCH_INIT_TREE = 'FETCH_INIT_TREE';
+export const FETCH_SERVICE = 'FETCH_SERVICE';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 
 export function plusPage() {
     return {type: PLUS_PAGE}
@@ -16,5 +19,14 @@ export function minusPage() {
 
 export function changeService(serviceID) {
     return {type: CHANGE_SERVICE, id: serviceID}
+}
+export function fetchInitTree(){
+    return {type: FETCH_INIT_TREE}
+}
+export function fetchServiceAction(serviceId){
+    return {type: FETCH_SERVICE, id: serviceId}
+}
+export function fetchSuccess(tree) {
+    return {type: FETCH_SUCCESS, tree: tree}
 }
 

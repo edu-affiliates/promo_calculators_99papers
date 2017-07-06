3d0e85b6ba60bc95289099bc2306007cdbc4b9ca
edu-affiliates <mango@plasticjam.com>
2017-07-06 12:29:19 +0300

    add inited to state

--- client/src/components/calculatorSmall/CalculatorSmallSelect.js
+++ client/src/components/calculatorSmall/CalculatorSmallSelect.js
@@ -3,15 +3,18 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {changeService} from './actions'
+import {changeService, fetchInitTree} from './actions'
 import CalculatorSmallDropdown from './CalculatorSmallDropdown'
 
-class CalculatorSmallInput extends React.Component {
+class CalculatorSmallSelect extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {openDropdown: false};
         this.openDropdown = this.openDropdown.bind(this);
+    }
+    componentWillMount(){
+        this.props.init();
     }
 
     openDropdown() {
@@ -21,43 +24,51 @@ class CalculatorSmallInput extends React.Component {
     }
 
     render() {
-        if (this.props.serviceComponent) {
-            return (
-                <div className="calc-sm-select-wrap ">
-                    <div onClick={this.openDropdown} className="calc-sm-select calc-sm-select--service">
-                        {this.props.service}
+        if(this.props.inited) {
+            if (this.props.serviceComponent) {
+                return (
+                    <div className="calc-sm-select-wrap ">
+                        <div onClick={this.openDropdown} className="calc-sm-select calc-sm-select--service">
+                            {this.props.service}
+                        </div>
+                        <CalculatorSmallDropdown serviceComponent={this.props.serviceComponent}
+                                                 open={this.state.openDropdown}/>
                     </div>
-                    <CalculatorSmallDropdown serviceComponent={this.props.serviceComponent} open={this.state.openDropdown}/>
-                </div>
-            )
-        }
-        if (this.props.levelComponent) {
-            return (
-                <div className="calc-sm-select-wrap">
-                    <div onClick={this.openDropdown} className="calc-sm-select calc-sm-select--level">{this.props.level}</div>
-                    <CalculatorSmallDropdown levelComponent={this.props.levelComponent} open={this.state.openDropdown}/>
-                </div>
+                )
+            }
+            if (this.props.levelComponent) {
+                return (
+                    <div className="calc-sm-select-wrap">
+                        <div onClick={this.openDropdown}
+                             className="calc-sm-select calc-sm-select--level">{this.props.level}</div>
+                        <CalculatorSmallDropdown levelComponent={this.props.levelComponent}
+                                                 open={this.state.openDropdown}/>
+                    </div>
 
-            )
-        }
-        if (this.props.deadlineComponent) {
-            return (
-                <div className="calc-sm-select-wrap">
-                    <div onClick={this.openDropdown} className="calc-sm-select calc-sm-select--deadline">{this.props.deadline}</div>
-                    <CalculatorSmallDropdown deadlineComponent={this.props.deadlineComponent} open={this.state.openDropdown}/>
-                </div>
-            )
-        }
+                )
+            }
+            if (this.props.deadlineComponent) {
+                return (
+                    <div className="calc-sm-select-wrap">
+                        <div onClick={this.openDropdown}
+                             className="calc-sm-select calc-sm-select--deadline">{this.props.deadline}</div>
+                        <CalculatorSmallDropdown deadlineComponent={this.props.deadlineComponent}
+                                                 open={this.state.openDropdown}/>
+                    </div>
+                )
+            }
+        } else return (<div></div>)
     }
 }
 
-CalculatorSmallInput.propTypes = {
+CalculatorSmallSelect.propTypes = {
     changeService: PropTypes.func.isRequired
 };
 
 //container to match redux state to component props and dispatch redux actions to callback props
 const mapStateToProps = state => {
     return {
+        inited: state.inited,
         service: state.current.service.name,
         level: state.current.level.name,
         deadline: state.current.deadline.name
@@ -66,10 +77,13 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        init: () => {
+            dispatch(fetchInitTree())
+        },
         changeService: () => {
             dispatch(changeService('2183'))
         }
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CalculatorSmallInput);
+export default connect(mapStateToProps, mapDispatchToProps)(CalculatorSmallSelect);

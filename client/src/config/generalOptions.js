50178b51ac48b11a7762c8634a386a4230dc59f1
edu-affiliates <mango@plasticjam.com>
2017-08-09 13:26:00 +0300

    fix saga.js after removing

--- client/src/config/generalOptions.js
+++ client/src/config/generalOptions.js
@@ -1,27 +1,25 @@
-let hostname = '99papers.com';
+let hostname = '99papers.com'
 
 let generalOptions = {
-    'hostname': hostname,
-    'website_id': 432,
-    'service_ids': '1674, 1675, 1673, 1690',
-    'apiMode': 'M',
-    'dev_mode': true,
-    'dsc': '',
-    'rid': 1228,
-};
+  'hostname': hostname,
+  'website_id': 432,
+  'service_ids': '1674, 1675, 1673, 1690',
+  'apiMode': 'M',
+  'dev_mode': true,
+  'dsc': '',
+  'rid': 1228,
+}
 
-
-if (!!window.eduOptions) {
-    Object.assign(generalOptions, window.eduOptions);
+if (window.eduOptions) {
+  Object.assign(generalOptions, window.eduOptions)
 }
 
 if (generalOptions.dev_mode) {
-    generalOptions.siteApiUrl = `https://devapi.${generalOptions.hostname}`;
-    generalOptions.siteMyUrl = `https://devmy.${generalOptions.hostname}`;
+  generalOptions.siteApiUrl = `https://devapi.${generalOptions.hostname}`
+  generalOptions.siteMyUrl = `https://devmy.${generalOptions.hostname}`
 } else {
-    generalOptions.siteApiUrl = `https://api.${generalOptions.hostname}`;
-    generalOptions.siteMyUrl = `https://my.${generalOptions.hostname}`;
+  generalOptions.siteApiUrl = `https://api.${generalOptions.hostname}`
+  generalOptions.siteMyUrl = `https://my.${generalOptions.hostname}`
 }
 
-
-export default generalOptions;
+export default generalOptions
--- client/src/store/sagas.js
+++ client/src/store/sagas.js
@@ -49,14 +49,13 @@ function getStats() {
         segment_id: generalOptions.segment_id,
         rid: generalOptions.rid,
         referrer_url: document.referrer,
-        origin_url: window.location.protocol + "//" +
+        origin_url: window.location.protocol + '//' +
         window.location.host +
         window.location.pathname +
         window.location.search +
         window.location.hash
-    };
+    }
     return Object.assign({}, clientParams, urlParams);
-
 };
 
 const stats = getStats();
@@ -65,116 +64,110 @@ function getXsrf() {
     return fetchXsrf()
         .then(
             (response) => {
-                return JSON.parse(response).info.token;
+                return JSON.parse(response).info.token
             },
             (fail) => {
-                console.log(fail);
+                console.log(fail)
             }
-        );
+        )
 }
-
 
 function getTree(services_id = generalOptions.service_ids) {
     return getData(services_id)
         .then(
             (response) => {
-                const treeJson = JSON.parse(response);
-                const tree = normalize(treeJson['info'], treeSchema);
+                const treeJson = JSON.parse(response)
+                const tree = normalize(treeJson['info'], treeSchema)
 
-                return tree;
+                return tree
             },
             (fail) => {
-                console.log(fail);
+                console.log(fail)
             }
-        );
+        )
 }
 
 function getDiscount(coupon) {
     return checkCoupon(coupon)
         .then(
             (response) => {
-                const dsc = JSON.parse(response).info.discount_amount;
-                return dsc / 100;
+                const dsc = JSON.parse(response).info.discount_amount
+                return dsc / 100
             },
             (fail) => {
             }
         )
 }
 
-
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
-/**process api call for the statistic **/
+/** process api call for the statistic **/
 
-
-
-function* sendStatictic() {
+function * sendStatictic() {
     try {
+
         if (generalOptions.apiMode === 'M') {
-            const xsrf = helper.getCookie("_xsrf");
+            console.log('send statistic');
+            const xsrf = helper.getCookie('_xsrf');
             if (!xsrf) {
                 const xsrf = yield call(getXsrf);
                 yield call(fetchStats, stats, xsrf);
-                helper.setCookie('_xsrf', xsrf, 13);
+                helper.setCookie('_xsrf', xsrf, 13)
             } else {
-                yield call(fetchStats, stats, xsrf);
+                yield call(fetchStats, stats, xsrf)
             }
         }
     } catch (e) {
     }
-
 }
-/**process api call for the coupon **/
+/** process api call for the coupon **/
 
-
-function* fetchCoupon() {
+function * fetchCoupon() {
     try {
         // const coupon = сookieCoupon();
-        const coupon = 'ESSAYFIRST15';
+        const coupon = 'ESSAYFIRST15'
         if (coupon) {
-            const dsc = yield call(getDiscount, coupon);
-            yield put(fetchSuccessDsc(dsc));
+            const dsc = yield call(getDiscount, coupon)
+            yield put(fetchSuccessDsc(dsc))
         }
-    }
-    catch (e) {
+    } catch (e) {
     }
 }
 
-/**process api call for the initial state **/
-function* fetchServiceTree(action) {
+/** process api call for the initial state **/
+function * fetchServiceTree(action) {
     try {
-        const treeLocalStorage = yield call(helper.getFromLocalStorage, 'tree');
-        const defaultId = generalOptions.service_ids.split(',')[0].trim();
+        const treeLocalStorage = yield call(helper.getFromLocalStorage, 'tree')
+        const defaultId = generalOptions.service_ids.split(',')[0].trim()
         if (treeLocalStorage) {
-            yield put(fetchSuccess(treeLocalStorage));
-            yield put(setInitService(defaultId));
+            yield put(fetchSuccess(treeLocalStorage))
+            yield put(setInitService(defaultId))
         } else {
-            const tree = yield call(getTree);
+            const tree = yield call(getTree)
 
-            yield call(helper.putToLocalStorage, 'tree', tree);
-            yield put(fetchSuccess(tree));
-            yield put(setInitService(defaultId));
+            yield call(helper.putToLocalStorage, 'tree', tree)
+            yield put(fetchSuccess(tree))
+            yield put(setInitService(defaultId))
         }
-
     } catch (e) {
-        yield put({type: "USER_FETCH_FAILED", message: e.message});
-        console.log(e);
+        yield put({type: 'USER_FETCH_FAILED', message: e.message})
+        console.log(e)
     }
 }
 
-/**process api call for the selected service **/
-function* fetchServiceSingle(action) {
+/** process api call for the selected service **/
+function * fetchServiceSingle(action) {
     try {
-        const currentTree = yield  select((state) => state.tree);
+        const currentTree = yield select((state) => state.tree)
         if (currentTree.service[action.id]) {
-            yield put(changeService(action.id, action.calcId));
+            yield put(changeService(action.id, action.calcId))
         } else {
-            const tree = yield call(getTree, action.id);
+            const tree = yield call(getTree, action.id)
             // console.log(tree);
-            yield put(fetchSuccessSingle(tree, action.id));
-            yield put(changeService(action.id, action.calcId));
+            yield put(fetchSuccessSingle(tree, action.id))
+            yield put(changeService(action.id, action.calcId))
         }
     } catch (e) {
-        yield put({type: "USER_FETCH_FAILED", message: e.message});
+        yield put({type: 'USER_FETCH_FAILED', message: e.message})
     }
 }
 
@@ -182,12 +175,11 @@ function* fetchServiceSingle(action) {
  Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
  Allows concurrent fetches of user.
  */
-function* mysaga() {
-    yield takeEvery(FETCH_STATISTIC, sendStatictic);
-    yield takeEvery(FETCH_COUPON, fetchCoupon);
-    yield takeEvery(FETCH_INIT_TREE, fetchServiceTree);
-    yield takeEvery(FETCH_SERVICE, fetchServiceSingle);
+function * mysaga() {
+    yield takeEvery(FETCH_STATISTIC, sendStatictic)
+    yield takeEvery(FETCH_COUPON, fetchCoupon)
+    yield takeEvery(FETCH_INIT_TREE, fetchServiceTree)
+    yield takeEvery(FETCH_SERVICE, fetchServiceSingle)
 }
 
-export default mysaga;
-
+export default mysaga
